64e8007f923c644ce8e90c718c8a51432f6cccc2
amulya25p <amulya.pawshekae@konverge.ai>
2022-08-24 17:42:32 +0530

    a

--- loops.py
+++ loops.py
@@ -1,5 +1,13 @@
-l=-1
+#for loop basics
+"""l=-1
 for i in [1,21,13,43,50]:
     if i>l:
         l=i
-print("the largest no is ", l)
+print("the largest no is ", l)"""
+# counting in loops
+d=[23,64,22,88,444,666,34,6,7,0]
+count=0
+for thing in d:
+    count+=1
+print("count of list == ", count)
+print(type(d))
