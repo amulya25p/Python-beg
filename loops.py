09e67afaa167840c3f4660c9cb1ec242a78795c5
amulya25p <amulya.pawshekae@konverge.ai>
2022-08-24 17:49:44 +0530

    average

--- loops.py
+++ loops.py
@@ -4,10 +4,24 @@ for i in [1,21,13,43,50]:
     if i>l:
         l=i
 print("the largest no is ", l)"""
+
+
 # counting in loops
-d=[23,64,22,88,444,666,34,6,7,0]
+"""d=[23,64,22,88,444,666,34,6,7,0]
 count=0
 for thing in d:
     count+=1
 print("count of list == ", count)
-print(type(d))
+print(type(d))"""
+
+#average
+"""l=[1,2,3,4,5,6,7,8,9,10]
+c=0
+sum=0
+for i in l:
+    c+=1
+    sum+=i
+print("average of list is ", sum/c)"""
+
+
+
